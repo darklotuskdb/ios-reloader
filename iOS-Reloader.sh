0a32ff6fcf7a5cd36f02f2e9608b59a3c7270938
Kamal Deep Bhati <th.kamalsinghrajput@gmail.com>
2023-07-25 03:07:26 +0530

    Update iOS-Reloader.sh

--- iOS-Reloader.sh
+++ iOS-Reloader.sh
@@ -18,13 +18,43 @@ echo -e '\e[31m
 \e[0m'
 echo -e "\e[33m  By Kamaldeep Bhati (@DarkLotusKDB) <3\n\e[0m"
 
+echo -e "\e[92m[+] The script will install the following tools and applications\e[0m"
+echo -e '
+A-Bypass
+AJB
+Choicy
+FlyJB X
+Hestia
+HideJB
+JailProtect
+KernBypass
+Liberty Lite
+PowerSelector
+Shadow
+Sniperbypassjb
+SSL Kill Switch 2
 
-echo -e "\e[92m[+] Taking backup of cydia.list as cydia.list.bak\e[0m"
+-------------------
+
+Filza
+CrackerXL+
+NewTerm 2
+PowerSelector
+vnodebypass
+Flex 3
+Sileo / Cydia
+
+'
+
+echo -e "\n[*] Press any key to continue..."
+read -n 1 -s -r -p ""
+
+echo -e "\n\e[92m[+] Taking backup of cydia.list as cydia.list.bak\e[0m"
 mv /etc/apt/sources.list.d/cydia.list /etc/apt/sources.list.d/cydia.list.bak &> /dev/null
 
 echo -e "\n\e[92m[+] Preparing the cydia.list\e[0m"
 
-echo -e "\ndeb https://apt.bingner.com/ ./\ndeb http://apt.modmyi.com/ stable main\ndeb http://apt.thebigboss.org/repofiles/cydia/ stable main\ndeb http://cydia.zodttd.com/repo/cydia/ stable main\ndeb http://julioverne.github.io/ ./\ndeb https://build.frida.re/ ./\ndeb https://cydia.akemi.ai/ ./\ndeb https://cydia.angelxwind.net/ ./\ndeb https://cydia.ichitaso.com/ ./\ndeb https://cydia.iphonecake.com/ ./\ndeb https://cydia.nowsecure.com/ ./\ndeb https://hackyouriphone.org/ ./\ndeb https://havoc.app/ ./\ndeb https://ios.jjolano.me/ ./\ndeb https://ios.tweaks.fun/ ./\ndeb https://jonlu.ca/repo/ ./\ndeb https://julioverne.github.io/ ./\ndeb https://level3tjg.xyz/repo/ ./\ndeb https://repo.chariz.com/ ./\ndeb https://repo.co.kr/ ./\ndeb https://repo.dynastic.co/ ./\ndeb https://repo.getsileo.app/ ./\ndeb https://repo.hackyouriphone.org/ ./\ndeb https://repo.packix.com/ ./\ndeb https://rpetri.ch/repo/ ./\ndeb https://ryleyangus.com/repo/ ./\n" | tee -a /etc/apt/sources.list.d/cydia.list.bak
+echo -e "\ndeb https://apt.bingner.com/ ./\ndeb http://apt.modmyi.com/ stable main\ndeb http://apt.thebigboss.org/repofiles/cydia/ stable main\ndeb http://cydia.zodttd.com/repo/cydia/ stable main\ndeb http://julioverne.github.io/ ./\ndeb https://build.frida.re/ ./\ndeb https://cydia.akemi.ai/ ./\ndeb https://cydia.angelxwind.net/ ./\ndeb https://cydia.ichitaso.com/ ./\ndeb https://cydia.iphonecake.com/ ./\ndeb https://cydia.nowsecure.com/ ./\ndeb https://hackyouriphone.org/ ./\ndeb https://havoc.app/ ./\ndeb https://ios.jjolano.me/ ./\ndeb https://ios.tweaks.fun/ ./\ndeb https://jonlu.ca/repo/ ./\ndeb https://julioverne.github.io/ ./\ndeb https://repo.chariz.com/ ./\ndeb https://repo.co.kr/ ./\ndeb https://repo.dynastic.co/ ./\ndeb https://repo.getsileo.app/ ./\ndeb https://repo.hackyouriphone.org/ ./\ndeb https://repo.packix.com/ ./\ndeb https://rpetri.ch/repo/ ./\ndeb https://ryleyangus.com/repo/ ./\ndeb https://mrepo.org/ ./\n" | tee -a /etc/apt/sources.list.d/cydia.list.bak
 
 cat /etc/apt/sources.list.d/cydia.list.bak | sort -u > /etc/apt/sources.list.d/cydia.list
 
@@ -33,7 +63,7 @@ apt-get update --fix-missing --allow-unauthenticated --allow-insecure-repositori
 
 echo -e "\n\e[92m[+] Please wait.... Installation in progress\e[0m"
 
-Pkg="com.tigisoftware.filza e.frida.server  com.icraze.hestia com.julioverne.sslkillswitch2 com.hackyouriphone.a-bypass odcctools darwintools com.ahmedmakls.ajb com.opa334.choicy com.thuthuatjb.hidejb ai.akemi.appsyncunified com.julioverne.jailprotect com.ryleyangus.libertylite.beta jp.akusio.kernbypass-unofficial me.jjolano.shadow com.bypassjb.sniper cydia.com.ipc.crackerxi com.ichitaso.powerselector11 ws.hbang.newterm2 kr.xsf1re.vnodebypass com.hackyouriphone.flyjb openssh libssh2 openssh-client openssh-global-listener openssh-server lldb lldb-10 liblldb-10 cycript python python3 gawk nano unzip coreutils tree git org.coolstar.sileo"
+Pkg="com.tigisoftware.filza re.frida.server  com.icraze.hestia com.julioverne.sslkillswitch2 com.hackyouriphone.a-bypass odcctools darwintools com.ahmedmakls.ajb com.opa334.choicy com.thuthuatjb.hidejb ai.akemi.appsyncunified com.julioverne.jailprotect com.ryleyangus.libertylite.beta jp.akusio.kernbypass-unofficial me.jjolano.shadow com.bypassjb.sniper cydia.com.ipc.crackerxi com.ichitaso.powerselector com.ichitaso.powerselector11 ws.hbang.newterm2 kr.xsf1re.vnodebypass org.mr.flyjbx openssh libssh2 openssh-client openssh-global-listener openssh-server lldb lldb-10 liblldb-10 cycript python python3 gawk nano unzip coreutils tree git org.coolstar.sileo libkrw com.johncoates.flex3"
 
 for i in ${Pkg} ;
  do apt-get install "${i}" -y --allow-unauthenticated
@@ -45,6 +75,11 @@ if [ -f "$install_log" ]; then
   if cat "$install_log" | grep -ia "Unable to locate package" &> /dev/null ; then
     echo -e "\n\e[31m[-] The following packages are not installed properly. Please install manually:\e[0m"
     cat "$install_log" | grep -ia "Unable to locate package"
+  
+  elif cat | grep -ia -A1 "The following packages have unmet dependencies" &> /dev/null ; then 
+    echo -e "\n\e[31m[-] The following packages are not installed because of the dependencies issue:\e[0m"
+    cat "$install_log" | grep -ia -A1 "The following packages have unmet dependencies"
+
   else
     echo -e "\n\e[92m[+] All the iOS tools installed correctly."
   fi
@@ -77,7 +112,3 @@ while true; do
 done
 
 echo -e "\e[92mThank You\e[0m"
-
-
-
-
