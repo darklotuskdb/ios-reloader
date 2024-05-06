66409b91db3237f626ca6466f6eecde5a9e48a4e
Kamaldeep Bhati <th.kamalsinghrajput@gmail.com>
2024-05-06 23:43:10 +0530

    Update iOS-Reloader.sh

--- iOS-Reloader.sh
+++ iOS-Reloader.sh
@@ -70,7 +70,7 @@ apt-get update --fix-missing --allow-unauthenticated --allow-insecure-repositori
 
 echo -e "\n\e[92m[+] Please wait.... Installation in progress\e[0m"
 
-Pkg="com.tigisoftware.filza re.frida.server com.icraze.hestia odcctools darwintools com.opa334.choicy ai.akemi.appsyncunified com.bypassjb.sniper com.ichitaso.powerselector ws.hbang.newterm2 openssh lldb cycript gawk nano unzip coreutils tree git com.johncoates.flex3 fun.tweaks.locationfaker com.cannathea.afc2d-arm64 com.gamegod.igg com.julioverne.sslkillswitch2 cydia.com.ipc.crackerxi"
+Pkg="com.tigisoftware.filza re.frida.server com.icraze.hestia odcctools darwintools com.opa334.choicy ai.akemi.appsyncunified com.bypassjb.sniper com.ichitaso.powerselector ws.hbang.newterm2 openssh lldb cycript gawk nano unzip coreutils tree git com.johncoates.flex3 fun.tweaks.locationfaker com.cannathea.afc2d-arm64 com.gamegod.igg libplist-utils com.julioverne.sslkillswitch2 cydia.com.ipc.crackerxi"
 
 for i in ${Pkg} ;
  do apt-get install -y "${i}" --allow-unauthenticated
@@ -151,7 +151,7 @@ apt-get update --allow-unauthenticated --allow-insecure-repositories
 
 echo -e "\n\e[92m[+] Please wait.... Installation in progress\e[0m"
 
-Pkg="com.tigisoftware.filza re.frida.server com.icraze.hestia odcctools darwintools com.opa334.choicy com.bypassjb.sniper ws.hbang.newterm3 openssh lldb cycript gawk nano unzip coreutils tree git fun.tweaks.locationfaker ai.akemi.appsyncunified com.cannathea.afc2d-arm64 com.gamegod.igg com.julioverne.sslkillswitch2"
+Pkg="com.tigisoftware.filza re.frida.server com.icraze.hestia odcctools darwintools com.opa334.choicy com.bypassjb.sniper ws.hbang.newterm3 openssh lldb cycript gawk nano unzip coreutils tree git fun.tweaks.locationfaker ai.akemi.appsyncunified com.cannathea.afc2d-arm64 com.gamegod.igg libplist-utils com.julioverne.sslkillswitch2"
 
 #removed: com.rpgfarm.a-bypass com.thuthuatjb.hidejb com.julioverne.jailprotect com.ryleyangus.libertylite.beta me.jjolano.shadow
 
